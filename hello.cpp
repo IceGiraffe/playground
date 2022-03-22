29d273eabdb1ae7ef83386f1e3ded1cf5f0f9c9d
Wang Feiyu <wangfeiyu@pku.edu.cn>
2022-03-22 12:13:59 +0800

    first commit

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <list>
+#include <vector>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <memory>
+#include <numeric>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
